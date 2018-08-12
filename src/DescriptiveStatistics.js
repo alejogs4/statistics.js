b3766e46ba6bdd393f5a767b1eafb02ab3f67170
alejogs4 <alejogs4@gmail.com>
2018-08-11 23:10:06 -0500

    Add quantile function

--- src/DescriptiveStatistics.js
+++ src/DescriptiveStatistics.js
@@ -29,7 +29,7 @@ class DescriptiveStatistics {
       return (this.sample[ Math.floor(length / 2) ] + this.sample[ Math.floor((length / 2) + 1) ]) / 2
     }
     else {
-      return this.sample[ (this.sample.length + 1) / 2 ]
+      return this.sample[ ((this.sample.length + 1) / 2) - 1 ]
     }
   }
 
@@ -39,6 +39,26 @@ class DescriptiveStatistics {
    */
   avg() {
     return this.sample.reduce((total, value) => total + value) / this.sample.length
+  }
+
+  /**
+   * Get the quantile of data set
+   * @param {Number} percentage shold be a number between 1 and 100
+   */
+  quantile(percentage) {
+    if(percentage > 100 || percentage < 1) {
+      // Here throw the error message if the percentage parameter is incorrect
+      throw new Error('The percentage value should be a number between 1 and 100')
+    }
+    else {
+      /**
+       * Get the i and h to return the quantile
+       */
+      const length = this.sample.length
+      const i = (((length - 1) * percentage) / 100) + 1
+      const h = Math.floor(i) - 1
+      return this.sample[h] + (this.sample[h + 1] - this.sample[h]) * (i - (h + 1))
+    }
   }
 
 }
