097b7f950a2e7f248580d57f19181bd1b106aed6
alejogs4 <alejogs4@gmail.com>
2018-08-19 22:17:03 -0500

    Add unit tests for measures of dispersion functions

--- test/descriptive.test.js
+++ test/descriptive.test.js
@@ -29,3 +29,38 @@ describe('Quantile', function() {
     assert.strictEqual(sample.quantile(35), 3.2, 'Should return 3.2')
   })
 })
+
+describe('IQR', function() {
+  it('Returns the difference between percentile 75 and 25', function() {
+    const sample = new DescriptiveStatistics([3,3,1,5,7,8,9])
+    assert.strictEqual(sample.iqr(), 4.5,'Should return 4.5')
+  })
+})
+
+describe('Range', function() {
+  it('Returns the difference between two xtreme values', function() {
+    const sample = new DescriptiveStatistics([3,3,1,5,7,8,9])
+    assert.strictEqual(sample.range(), 8, 'Should returns 8')
+  })
+})
+
+describe('Variance', function() {
+  it('Returns the variance of the elements respect their average', function() {
+    const sample = new DescriptiveStatistics([3,3,1,5,7,8,9])
+    assert.strictEqual(sample.variance(), 8.809523809523808, 'Should returns 8.80952381')
+  })
+})
+
+describe('Standard deviation', function() {
+  it('Returns the standard deviation that is equal to square root of variance', function() {
+    const sample = new DescriptiveStatistics([3,3,1,5,7,8,9])
+    assert.strictEqual(sample.standardDeviation(), 2.9680841985233184, 'Should return the standard deviation')
+  })
+})
+
+describe('variation coefficient', function() {
+  it('Get how many times the standard deviation is in the average', function() {
+    const sample = new DescriptiveStatistics([3,3,1,5,7,8,9])
+    assert.strictEqual(sample.variationCoefficient(), 57.71274830462008, 'Should return 57.71274830462008')
+  })
+})
